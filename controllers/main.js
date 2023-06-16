c117f7f6155bce97738a3f8ca8d92be3142e166b
Abdullah <abdullah.khan171202@gmail.com>
2023-06-17 02:56:08 +0530

    project completed

---
+++ controllers/main.js
@@ -0,0 +1,31 @@
+const { BadRequestError } = require('../errors')
+const jwt = require('jsonwebtoken')
+
+const login = async (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    throw new BadRequestError('Please provide username and password')
+  }
+
+  // just for demo, id is provided by DB
+  const id = new Date().getDate()
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+
+  res.status(200).json({ msg: 'user created', token })
+}
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100)
+  res.status(200).json({
+    msg: `hello ${req.user.username}`,
+    secret: `your authorized data is this secret key: ${luckyNumber}`,
+  })
+}
+
+module.exports = {
+  login,
+  dashboard,
+}
